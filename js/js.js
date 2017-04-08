9d47340110e9696647e8d7dbfe64ad48410b3482
Doug Dryburgh <douglas.dryburgh@gmail.com>
2017-04-07 17:28:20 -0700

    Completely revamped the site to make it more modern and professional looking

--- js/js.js
+++ js/js.js
@@ -1,7 +1,6 @@
 $(document).on('click', 'a', function(event){
-   // event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+    }, 750);
 });
